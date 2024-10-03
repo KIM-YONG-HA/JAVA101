ad01860fac4aad7693022b15fedfbe4a4be89fb6
KIM-YONG-HA <vb901217@gmail.com>
2024-10-03 23:12:09 +0900

    파일 수정

--- JAVA101/src/j240930/J101_4_VariableReplacement.java
+++ JAVA101/src/j240930/J101_4_VariableReplacement.java
@@ -4,15 +4,16 @@ public class J101_4_VariableReplacement {
 
 	public static void main(String[] args) {
 		
-		/*****
+		/*
 		 
-		요구사항
 		변수 x, y의 값을 서로 교체하여 출력하기
 		
-		*****/
+		*/
 		
 		int x = 1;
 		int y = 2;
+		
+		System.out.println(x+", "+y);
 		
 		int tmp = x;
 		x = y;
