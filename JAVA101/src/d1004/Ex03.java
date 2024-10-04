d3e8448e33595f810830796302ffb42bb93d8932
DESKTOP-2DK9R40\kj <vb901217@gmail.com>
2024-10-04 17:00:04 +0900

    마지막교시 커밋

--- JAVA101/src/d1004/Ex03.java
+++ JAVA101/src/d1004/Ex03.java
@@ -2,24 +2,21 @@ package d1004;
 
 public class Ex03 {
 
-	public static void main(String[] args) {
-		
-		
-		
+	public static void main(String[] args) throws Exception {
 		
 		// printf 
 		// conversion : d, s, f
 		
-		
 		// %[argument_index$]
 		
-		// 
+		// 콘솔 출력 : System.out
+		// 콘솔 입력 : System.in
 		
+		// 예외처리 : 메소드 뒤 throws Exception
 		
-		
-		
-		
-
+		int keyCode;
+		keyCode = System.in.read();
+		System.out.println("key code : " + keyCode);
 
 	}
 
