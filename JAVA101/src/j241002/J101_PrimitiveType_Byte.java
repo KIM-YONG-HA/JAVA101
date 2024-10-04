8b2ceba6c2ef09b3ba5beab936ed05863d259d9d
DESKTOP-2DK9R40\kj <vb901217@gmail.com>
2024-10-04 13:53:52 +0900

    primitive type 1차 커밋

---
+++ JAVA101/src/j241002/J101_PrimitiveType_Byte.java
@@ -0,0 +1,42 @@
+package j241002;
+
+public class J101_PrimitiveType_Byte {
+
+	public static void main(String[] args) {
+		
+		// 기본형 타입(Primitive Type), 8개 
+		
+		// 정수형(Integer) : byte, short, char, int, long 
+		// 실수형 : float, double 
+		// 논리형(Boolean) : boolean  
+		
+		
+		// byte
+		// 메모리 : 1Byte(8bit)
+		// 범위 : -2^7 ~ (2^7-1) / -128 ~ 127
+		
+		byte by1 = -128;
+		byte by2 = -30;
+		byte by3 = 0;
+		byte by4 = 30;
+		byte by5 = 127;
+		//byte var6 = 128;
+		
+		System.out.println(by1);
+		System.out.println(by2);
+		System.out.println(by3);
+		System.out.println(by4);
+		System.out.println(by5);
+		//System.out.println(by6);
+		
+		/*
+		  
+		  Description	Resource	Path	Location	Type
+		  Type mismatch: cannot convert from int to byte	ByteExample.java	/chap01/src/d1002	line 14	Java Problem
+		  
+		 */
+		
+
+	}
+
+}
