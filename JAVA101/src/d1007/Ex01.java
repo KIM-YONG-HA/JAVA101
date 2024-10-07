e14f0f6dfd0fca2c898974b4e0d4595a9cd715dd
DESKTOP-2DK9R40\kj <vb901217@gmail.com>
2024-10-07 15:01:06 +0900

    operator, char 파일 수정

---
+++ JAVA101/src/d1007/Ex01.java
@@ -0,0 +1,91 @@
+package d1007;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		
+		// 연산자(Operator)
+		
+		// 산술 부호 문자열 대입 증감 비교 논리 조건 
+		
+		
+		
+		
+		// 우선순위
+		
+		// 단항->이항->삼항
+		// 산술->비교->논리->대입
+		
+		
+		// 대부분 좌에서 우
+		// 우선순위 같다면 왼쪽부터 수행
+		// 100 / 2 * 3 % 5
+		
+		
+		// 증감, 대입은 우에서 좌로 
+		// int a += 1; int a = a + 1
+		// a = b = c = 5
+		
+		
+		// 논리곱 a && b => true 1 x false 0 
+		
+		
+		
+		
+		// 단항연산자 ++x;
+		// 이항연산자 x + y;
+		// 삼항연산자 (sum > 90) ? A : B;
+		
+		
+		
+		
+		
+		
+		
+		// 증감연산자 
+		
+		int a = 10;
+		int b = 10;
+		int rs1 = ++a + 10;
+		int rs2 = b++ + 10;
+		
+		
+		System.out.println(rs1);
+		System.out.println(rs2);
+		System.out.println(b);
+		
+		
+//		a++; // 다른 연산 수행 후 피연산자 1증가
+		
+//		++a; // 다른 연산 수행 전 피연산자 1증가
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
