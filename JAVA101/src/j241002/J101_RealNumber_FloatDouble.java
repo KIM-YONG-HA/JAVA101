4c3c9181975b4cb407880000d7bcf08c88e05022
DESKTOP-2DK9R40\kj <vb901217@gmail.com>
2024-10-08 14:09:51 +0900

    수업 시작 전 커밋

--- JAVA101/src/j241002/J101_RealNumber_FloatDouble.java
+++ JAVA101/src/j241002/J101_RealNumber_FloatDouble.java
@@ -17,6 +17,18 @@ public class J101_RealNumber_FloatDouble {
 		// 정밀도 : 소수점 이하 약 15자리 (15~18)
 		
 		
+		// 실수값 저장 
+		//float var1 = 3.14; // type mismatch
+		float var2 = 3.14f;
+		double var3 = 3.14; // float 보다 약 2배정도 정밀도가 높아서 double 
+		
+		// 정밀도 테스트 
+		float var4 = 0.1234567890123456789f;
+		double var5 = 0.1234567890123456789;
+		
+		
+		System.out.println();
+		
 		
 
 	}
