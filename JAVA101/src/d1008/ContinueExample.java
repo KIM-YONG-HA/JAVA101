0b1caa6f23eb1e960f9c9cf6a5bb432ac45c7e36
DESKTOP-2DK9R40\kj <vb901217@gmail.com>
2024-10-08 18:06:12 +0900

    star pattern printing 예제 시작

---
+++ JAVA101/src/d1008/ContinueExample.java
@@ -0,0 +1,23 @@
+package d1008;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i= 1; i <= 10; i++) {
+			
+			
+			if(i%2 != 0) continue; // odd continue
+			
+			System.out.println(i);
+			
+			
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ JAVA101/src/starPatternPrinting/Step01_RightAngledTriangle.java
@@ -0,0 +1,92 @@
+package starPatternPrinting;
+
+public class Step01_RightAngledTriangle {
+
+	public static void main(String[] args) {
+		
+		
+//		직각삼각형 (Right-Angled Triangle)
+//		*
+//		**
+//		***
+//		****
+//		*****
+		
+//		역직각삼각형 (Inverted Right-Angled Triangle)
+//		*****
+//		****
+//		***
+//		**
+//		*
+
+//		오른쪽 정렬 직각삼각형 (Right-Aligned Right-Angled Triangle)
+//		*
+//		**
+//		***
+//		****
+//		*****
+
+//		왼쪽 정렬 직각삼각형 (Left-Aligned Right-Angled Triangle)
+//		*****
+//		****
+//		***
+//		**
+//		*
+
+//		피라미드 (Pyramid)
+//	    *
+//	   ***
+//	  *****
+//	 *******
+//	*********
+
+//	역피라미드 (Inverted Pyramid)
+//	*********
+//	 *******
+//	  *****
+//	   ***
+//	    *
+
+//	다이아몬드 (Diamond)
+//	    *
+//	   ***
+//	  *****
+//	 *******
+//	*********
+//	 *******
+//	  *****
+//	   ***
+//	    *
+
+//	빈 사각형 (Hollow Rectangle)
+//	*********
+//	*       *
+//	*       *
+//	*       *
+//	*********
+
+//	별 계단 (Star Staircase)
+//	*
+//	**
+//	***
+//	****
+//	*****
+//	******
+
+//	나무 모양 (Tree Shape)
+//	    *
+//	   ***
+//	  *****
+//	 *******
+//	*********
+//	    |
+
+		
+		
+		
+		
+		
+
+	}
+
+}
