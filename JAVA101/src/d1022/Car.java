00fa7f050b16d127a3f1dc3dc55f5cc3250d6373
DESKTOP-2DK9R40\kj <vbsic5@naver.com>
2024-10-22 15:51:05 +0900

    생성자 호출

--- JAVA101/src/d1022/Car.java
+++ JAVA101/src/d1022/Car.java
@@ -7,23 +7,49 @@ public class Car {
 	String color;
 	int maxSpeed;
 
+// 중복제거 전 
+//	
+//	public Car() {
+//
+//	}
+//
+//	public Car(String model) {
+//		this.model = model;
+//	}
+//
+//	public Car(String model, String color) {
+//		this.model = model;
+//		this.color = color;
+//	}
+//
+//	public Car(String model, String color, int maxSpeed) {
+//		this.model = model;
+//		this.color = color;
+//		this.maxSpeed = maxSpeed;
+//	}
+
+	
+	
+	
+	
+	// this() 자기자신 생성자 호출
+	// 생성자 내 첫번째 줄에서만 사용가능
+	
+	
+	// 중복 제거 후 
 	public Car() {
 
 	}
 
 	public Car(String model) {
-		super();
-		this.model = model;
+		this(model, null, 0);
 	}
 
 	public Car(String model, String color) {
-		super();
-		this.model = model;
-		this.color = color;
+		this(model, color, 0);
 	}
 
 	public Car(String model, String color, int maxSpeed) {
-		super();
 		this.model = model;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
