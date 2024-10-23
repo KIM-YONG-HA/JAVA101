1c6246fdac95f8e694a269f1048327eb8f1577c4
DESKTOP-2DK9R40\kj <vbsic5@naver.com>
2024-10-23 09:21:59 +0900

    241023 자바 1교시 시작

---
+++ JAVA101/src/d1023/MainClass.java
@@ -0,0 +1,21 @@
+package d1023;
+
+public class MainClass {
+
+	// 메인 메소드는 1개만
+	// 소스가 길어지면 분리
+	// public static : 정적, 하나에 클래스에서만 가능 
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Test {
+
+}
