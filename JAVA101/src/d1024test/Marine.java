e08af3f6429bcb379cbf9069c052f31b835039f6
DESKTOP-2DK9R40\kj <vbsic5@naver.com>
2024-10-24 17:14:36 +0900

    패키지 내 파일 위치 변경

--- JAVA101/src/d1024test/Marine.java
+++ JAVA101/src/d1024test/Marine.java
@@ -1,24 +1,24 @@
-package d1023test;
+package d1024test;
 
 
-class Marine_t{
+class Marine {
 	// 필드
 	private int x, y, hp;  // hp는 체력
 	private static int weapon, armor;   // weapon는 공격력, armor는 방어력
 	
 	// 생성자
-	public Marine_t() {	}
+	public Marine() {	}
 	
-	public Marine_t(int x, int y) {
+	public Marine(int x, int y) {
 		this(x, y, 100, 1, 1);
 	}
-	public Marine_t(int x, int y, int hp) {
+	public Marine(int x, int y, int hp) {
 		this(x, y, hp, 1, 1);
 	}
-	public Marine_t(int x, int y, int hp, int weapon) {
+	public Marine(int x, int y, int hp, int weapon) {
 		this(x, y, hp, weapon, 1);
 	}
-	public Marine_t(int x, int y, int hp, int weapon, int armor) {
+	public Marine(int x, int y, int hp, int weapon, int armor) {
 		this.x=x; this.y=y; this.hp=hp; Marine.weapon=weapon;
 		Marine.armor=armor;
 	}
@@ -113,46 +113,6 @@ class Marine_t{
 	}
 }
 
-public class Marine_t {
 
-	public static void main(String[] args) {
-		System.out.println("***   마린 생성 시작");
-		Marine m1=new Marine();
-		System.out.println(m1);
-		Marine m2=new Marine(2, 2);
-		System.out.println(m2);
-		Marine m3=new Marine(3, 3, 60);
-		System.out.println(m3);
-		Marine m4=new Marine(4, 4, 50, 1);
-		System.out.println(m4);
-		Marine m5=new Marine(5, 5, 70, 1, 1);
-		System.out.println(m5);
-		
-		System.out.println("***  방어력 증가");
-		Marine.armorUp();
-		System.out.println(m1);
-		System.out.println(m2);
-		System.out.println(m3);
-		System.out.println(m4);
-		System.out.println(m5);
-		
-		System.out.println("***   공격력 증가");
-		Marine.weaponUp();
-		System.out.println(m1);
-		System.out.println(m2);
-		System.out.println(m3);
-		System.out.println(m4);
-		System.out.println(m5);
-		
-		System.out.println("***  마린 hp 감소");
-		m1.hpDown();
-		System.out.println(m1);
-		m2.hpDown();
-		System.out.println(m2);
-		
-		System.out.println("*** m1 마린 위치 변경");
-		m1.marineMove(4, 6);
-		System.out.println(m1);
-	}
 
-}
+
