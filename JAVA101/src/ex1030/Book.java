0cb870042ec241e1b781872a329885ebfd80c9d9
DESKTOP-2DK9R40\kj <vbsic5@naver.com>
2024-10-30 17:16:41 +0900

    241030 자바 객체배열 예제

---
+++ JAVA101/src/ex1030/Book.java
@@ -0,0 +1,26 @@
+package ex1030;
+
+public class Book {
+	
+	
+	String title;
+	int price;
+	String writer;
+	
+	
+	public Book() {
+		
+	}
+	
+
+
+	void showBook() {
+		
+		System.out.printf("%s의 가격은 %d원이고 저자는 %s입니다. %n",title, price, writer);
+		
+	}
+	
+	
+	
+
+}
